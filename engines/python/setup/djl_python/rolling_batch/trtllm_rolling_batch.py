6b6581974b8bfb2ba601c4463c721289f7c4eaaa
Robert Yang <141875536+ydm-amazon@users.noreply.github.com>
2023-11-01 09:52:01 -0700

    python to mpi in serving properties

--- engines/python/setup/djl_python/rolling_batch/trtllm_rolling_batch.py
+++ engines/python/setup/djl_python/rolling_batch/trtllm_rolling_batch.py
@@ -30,7 +30,7 @@ class TRTLLMRollingBatch(RollingBatch):
         super().__init__(-1, **kwargs)
         # self.core = tritontoolkit.init_triton(model_id_or_path)
         # self.model = self.core.load_model("tensorrt_llm")
-        self.model = tensorrt_llm_toolkit.init_inference(model_id_or_path) # not sure kwargs okay or not
+        self.model = tensorrt_llm_toolkit.init_inference(model_id_or_path) # not sure kwargs okay or not update kwargs needed
         self.tokenizer = AutoTokenizer.from_pretrained(model_id_or_path, padding_side="left", revision=kwargs.get('revision', None))
         self.request_cache = OrderedDict()
 
